5a711f2b3ceeb2b344509c5c533d01ad2d7a115f
Ben Fanning fanninggg@gmail.com <fanninggg@gmail.com>
2020-11-12 17:57:07 +0000

    Mr Cocktail Live Code

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+require 'open-uri'
+require 'json'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+ingredients['drinks'].each do |ingredient|
+	Ingredient.create(name: ingredient['strIngredient1'])
+end
+Ingredient.create(name: "Egg White")
+
+puts "Finished"
